c69401e98d9627cfd5d65076e089aeb239ebaa5b
maximzadoina <maxim.zadoina@gmail.com>
2020-07-02 23:35:48 +0200

    m

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-console.log(uri+'test uri')
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -26,6 +26,7 @@ const showcasesRouter=require('./routes/showcases')
 const articlesRouter=require('./routes/articles')
 app.use('/showcases',showcasesRouter);
 app.use('/articles',articlesRouter);
+app.use(express.static('../frontend/build'))
 
 app.listen(port, () => {
     console.log(`server started on port: ${port}`)
---
+++ frontend/node_modules/react-fully-scrolled/lib/Scroller/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _Scroller = require('./Scroller');
+
+var _Scroller2 = _interopRequireDefault(_Scroller);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _Scroller2.default;
+module.exports = exports['default'];
